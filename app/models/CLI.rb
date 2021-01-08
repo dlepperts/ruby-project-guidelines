bfaf1ac63d3333c6e4420929e2cf5dee41aa484a
dlepperts <danielle.leppertsimenauer@gmail.com>
2021-01-08 09:20:41 -0600

    Friday changes

--- app/models/CLI.rb
+++ app/models/CLI.rb
@@ -48,6 +48,7 @@ class CLI
     end
 
     def patient_home 
+
         clear_screen
         puts "Hi #{@current_user.username}, you have #{@current_user.sessions.count} completed sessions"
         divider
@@ -71,53 +72,83 @@ class CLI
             puts 'How would you like to find a therapist??'
             puts 'M) Find someone who has the exact same identities'
             puts 'C) Find someone with custom identities'
-    
-
-            #puts 'How would you like to narrow your search?'
-            #puts 'GI) Gender Identity'
-            #puts 'SI) Sexual Identity'
-            #puts 'SB) Sex at Birth'
-            #puts 'R) Race'
-            #puts 'A) Age'
-            #puts 'E) Education'
-            #puts 'C) Certifications'
 
             search_choice = get_user_input.upcase
+
+            puts '*' * 30
 
             case search_choice
             when 'M'
                 puts 'Here are the therapists who have the same identities as you'
                 puts print_therapist_names_matching_identity
             when 'C'
-                puts 'What gender identity would you like in a therapist?'
-                puts 'Woman'
-                puts 'Man'
-                puts 'Non-Binary'
-                #puts 'No preference'
-                gender_identity_preference = get_user_input.upcase
+                do_you_have_a_gender_identity_preference?
+                gender_choice = get_user_input.upcase
+                case gender_choice
+                when 'Y'
+                    get_gender_identity_preference
+                    do_you_have_a_sexual_identity_preference?
+                    sex_choice = get_user_input.upcase
+                    case sex_choice
+                    when 'Y'
+                        get_sexual_identity_preference
+                        do_you_have_a_race_preference?
+                        race_choice = get_user_input.upcase
+                        case race_choice
+                        when 'Y'
+                            get_race_preference
+                            @therapists = find_therapists_in_my_location_who_take_my_insurance.select{|therapist| therapist.gender_id == @gender_identity_preference and therapist.sexual_id == @sexual_identity_preference and therapist.race == @race_preference}
+                            puts @therapists.map{|therapist| "Name: #{therapist.chosen_name}, Gender Identity: #{therapist.gender_id}, Sexual identity: #{therapist.sexual_id}, Race: #{therapist.race}, Age: #{therapist.age}"}
+                        when 'N'
+                            @therapists = find_therapists_in_my_location_who_take_my_insurance.select{|therapist| therapist.gender_id == @gender_identity_preference and therapist.sexual_id == @sexual_identity_preference}
+                            puts @therapists.map{|therapist| "Name: #{therapist.chosen_name}, Gender Identity: #{therapist.gender_id}, Sexual identity: #{therapist.sexual_id}, Race: #{therapist.race}, Age: #{therapist.age}"}
+                        end
+                     when 'N'
+                        do_you_have_a_race_preference?
+                        race_choice = get_user_input.upcase
+                        case race_choice
+                        when 'Y'
+                            get_race_preference
+                            @therapists = find_therapists_in_my_location_who_take_my_insurance.select{|therapist| therapist.gender_id == @gender_identity_preference and therapist.race == @race_preference}
+                            puts @therapists.map{|therapist| "Name: #{therapist.chosen_name}, Gender Identity: #{therapist.gender_id}, Sexual identity: #{therapist.sexual_id}, Race: #{therapist.race}, Age: #{therapist.age}"}
+                        when 'N'
+                            @therapists = find_therapists_in_my_location_who_take_my_insurance.select{|therapist| therapist.gender_id == @gender_identity_preference}
+                            puts @therapists.map{|therapist| "Name: #{therapist.chosen_name}, Gender Identity: #{therapist.gender_id}, Sexual identity: #{therapist.sexual_id}, Race: #{therapist.race}, Age: #{therapist.age}"}
+                        end
+                    end
+                when 'N'
+                    do_you_have_a_sexual_identity_preference?
+                    sex_choice = get_user_input.upcase
+                    case sex_choice
+                    when 'Y'
+                        get_sexual_identity_preference
+                        do_you_have_a_race_preference?
+                        race_choice = get_user_input.upcase
+                        case race_choice
+                        when 'Y'
+                            get_race_preference
+                            @therapists = find_therapists_in_my_location_who_take_my_insurance.select{|therapist| therapist.sexual_id == @sexual_identity_preference and therapist.race == @race_preference}
+                            puts @therapists.map{|therapist| "Name: #{therapist.chosen_name}, Gender Identity: #{therapist.gender_id}, Sexual identity: #{therapist.sexual_id}, Race: #{therapist.race}, Age: #{therapist.age}"}
+                        when 'N'
+                            @therapists = find_therapists_in_my_location_who_take_my_insurance.select{|therapist| therapist.sexual_id == @sexual_identity_preference}
+                            puts @therapists.map{|therapist| "Name: #{therapist.chosen_name}, Gender Identity: #{therapist.gender_id}, Sexual identity: #{therapist.sexual_id}, Race: #{therapist.race}, Age: #{therapist.age}"}
+                        end
 
-                puts 'What sexual identity would you like in a therapist?'
-                puts 'Straight'
-                puts 'Gay'
-                puts 'Bisexual'
-                puts 'Lesbian'
-                puts 'Asexual'
-                puts 'Pansexual'
-                puts 'No preference'
-                sexual_identity_preference = get_user_input.upcase
-
-                puts 'What race would you like in a therapist?'
-                puts 'White'
-                puts 'Black'
-                puts 'Asian'
-                puts 'Latino'
-                puts 'Native American'
-                puts 'No preference'
-                race_preference = get_user_input.upcase
-
-                therapists = find_therapists_in_my_location_who_take_my_insurance.select{|therapist| therapist.gender_id == gender_identity_preference and therapist.sexual_id == sexual_identity_preference and therapist.race == race_preference}
-                therapists.map{|therapist| "name: #{therapist.chosen_name}, gender identity: #{therapist.gender_id}, sexual identity: #{therapist.sexual_id}, race: #{therapist.race}, age: #{therapist.age}"}
-
+                    when 'N'
+                        do_you_have_a_race_preference?
+                        race_choice = get_user_input.upcase
+                        case race_choice
+                        when 'Y'
+                            get_race_preference
+                            @therapists = find_therapists_in_my_location_who_take_my_insurance.select{|therapist| therapist.race == @race_preference}
+                            puts @therapists.map{|therapist| "Name: #{therapist.chosen_name}, Gender Identity: #{therapist.gender_id}, Sexual identity: #{therapist.sexual_id}, Race: #{therapist.race}, Age: #{therapist.age}"}
+                        when 'N'
+                            puts print_therapist_names_insurance_and_location
+                        end
+                    end
+                else
+                    incorrect_selection
+                end
             else
                 incorrect_selection
             end
@@ -132,9 +163,60 @@ class CLI
         end
     end
 
-    def find_therapist(gender_search_choice)
-        find_therapists_in_my_location_who_take_my_insurance.select{|therapist| therapist.gender_id = gender_search_choice}
+    def do_you_have_a_gender_identity_preference?
+        puts 'Do you have a gender preference?'
+        puts 'Y) Yes'
+        puts 'N) No'
     end
+
+    def do_you_have_a_sexual_identity_preference?
+        puts 'Do you have a sexual identity preference?'
+        puts 'Y) Yes'
+        puts 'N) No'
+    end
+
+    def do_you_have_a_race_preference?
+        puts 'Do you have a preference of what race your therapist is?'
+        puts 'Y) Yes'
+        puts 'N) No'
+    end
+
+    def get_gender_identity_preference
+        puts 'What gender identity would you like in a therapist?'
+        puts 'Woman'
+        puts 'Man'
+        puts 'Non-Binary'
+        puts 'Other'
+        @gender_identity_preference = get_user_input.capitalize
+        puts '*' * 30
+    end
+
+    def get_sexual_identity_preference
+        puts 'What sexual identity would you like in a therapist?'
+        puts 'Straight'
+        puts 'Gay'
+        puts 'Bisexual'
+        puts 'Lesbian'
+        puts 'Asexual'
+        puts 'Pansexual'
+        puts 'No Preference'
+        @sexual_identity_preference = get_user_input.capitalize
+        puts '*' * 30
+    end
+
+    def get_race_preference
+        puts 'What race would you like in a therapist?'
+        puts 'White'
+        puts 'Black'
+        puts 'Asian'
+        puts 'Latino'
+        puts 'Native American'
+        puts 'Other'
+        puts 'No Preference'
+        @race_preference = get_user_input.capitalize
+        puts '*' * 30
+    end
+
 
     def find_all_therapists
         Therapist.all
@@ -142,7 +224,7 @@ class CLI
 
     def print_therapist_names_insurance_and_location
         therapists = find_therapists_in_my_location_who_take_my_insurance
-        therapists.map{|therapist| "name: #{therapist.chosen_name}, gender identity: #{therapist.gender_id}, sexual identity: #{therapist.sexual_id}, race: #{therapist.race}, age: #{therapist.age}"}
+        therapists.map{|therapist| "Name: #{therapist.chosen_name}, Gender Identity: #{therapist.gender_id}, Sexual Identity: #{therapist.sexual_id}, race: #{therapist.race}, age: #{therapist.age}"}
     end
 
     def print_therapist_names_matching_identity
@@ -162,7 +244,7 @@ class CLI
         clear_screen
         puts "Oops try again..."
         pause
-        home
+        patient_home
     end
 
 end
